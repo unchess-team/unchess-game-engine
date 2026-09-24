638477d7b9c016d86dd2f48d91f849561b58f1c9
Ivan Burov <iiburov@proton.me>
2026-09-24 23:58:31 +0300

    feat: add square (#2)
    * feat: add square
    
    square is index of square from 0 to 255, 16*16 is max supported board size
    
    * fix: fix wrong int size

---
+++ include/engine/core/Square.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cstdint>
+
+class Square {
+public:
+    
+    using Index = std::uint8_t;
+
+    constexpr Square() = default;
+
+    explicit constexpr Square(Index index) : index_(index) {}
+
+    constexpr Index index() const {
+        return index_;
+    }
+
+    constexpr bool operator==(const Square& other) const {
+        return index_ == other.index_;
+    }
+
+private:
+    Index index_{0};
+};
